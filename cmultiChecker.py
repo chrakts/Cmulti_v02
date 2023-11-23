b169ef889e4b8b6ae103df7a186c3f1d753afe30
chrak <kaufen@cmja.de>
2023-11-23 22:50:56 +0100

    2

--- cmultiChecker.py
+++ cmultiChecker.py
@@ -6,7 +6,7 @@ cm = cmulti.CMULTI("CP", "/dev/ttyUSB0")
 while True:
     message = cm._readline()
     try:
-        text = message.decode('utf-8', 'backslashreplace')
+        text = message.decode('ascii', 'backslashreplace')
     except:
         text = message.decode('utf-8', "ignore").replace('^M', '\n\r') + "++++"
     with open("test.txt", "a") as myfile:
