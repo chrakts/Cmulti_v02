ebf63c682e20b501c2eb710e6a26c4e6b6ca29b0
chrak <kaufen@cmja.de>
2023-11-23 21:54:34 +0100

    .

--- cmultiChecker.py
+++ cmultiChecker.py
@@ -7,6 +7,6 @@ while True:
     try:
         text = message.decode('utf-8')
     except:
-        text = message
+        text = message[1:].decode('utf-8')
     with open("test.txt", "a") as myfile:
         myfile.write(text)
