7bc25668f66f2e4f1e6382a680951621aefc8b03
chrak <kaufen@cmja.de>
2023-11-24 19:49:59 +0100

    Exception entfernt

--- cmultiChecker.py
+++ cmultiChecker.py
@@ -6,10 +6,7 @@ cm = cmulti.CMULTI("CP", "/dev/ttyUSB0")
 
 while True:
     message = cm._readline()
-    try:
-        text = message.decode('ascii', 'backslashreplace').encode().decode('utf-8')
-    except:
-        text = message.decode('utf-8', "ignore").replace('^M', '\n\r') + "++++"
+    text = message.decode('ascii', 'backslashreplace').encode().decode('utf-8')
     with codecs.open("test.txt", "a", "utf-8") as myfile:
         myfile.write(datetime.datetime.now().ctime() + ' : ' + text)
 
